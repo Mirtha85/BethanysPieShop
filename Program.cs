2d844abe94ad146d0c9ab942587e32d570e5d4f4
Mirtha Jimenez <mirtha.jimenez.leon@gmail.com>
2026-03-12 18:50:53 -0400

    working with Entity Framework, creating my database, using migrations.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
 using BethanysPieShop.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
-builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IPieRepository, PieRepository>();
+
+builder.Services.AddDbContext<BethanysPieShopDbContext>(options => {
+    options.UseSqlite(
+        builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
+});
 
 var app = builder.Build();
 
@@ -17,5 +23,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseStaticFiles();
 app.MapDefaultControllerRoute();
+DbInitializer.Seed(app);
 app.Run();
 
