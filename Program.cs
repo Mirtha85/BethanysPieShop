033172df3357b511ba0c8068460084c8a0d458a2
Mirtha Jimenez <mirtha.jimenez.leon@gmail.com>
2026-03-15 18:35:48 -0400

    updating  views and main layout

--- Program.cs
+++ Program.cs
@@ -22,7 +22,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseStaticFiles();
-app.MapDefaultControllerRoute();
+app.MapDefaultControllerRoute();//{controller=Home}/{action=Index}/{id?}
+
+
+// app.MapControllerRoute(
+//     name: "default",
+//     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 DbInitializer.Seed(app);
 app.Run();
 
