d99801305793e5238391be18dbfcfeb766235520
Mirtha Jimenez <mirtha.jimenez.leon@gmail.com>
2026-03-21 23:43:48 -0400

    adding  shopping cart functionality.

--- Program.cs
+++ Program.cs
@@ -8,6 +8,11 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IPieRepository, PieRepository>();
 
+
+builder.Services.AddScoped<IShoppingCart, ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+builder.Services.AddSession(); 
+builder.Services.AddHttpContextAccessor();
+
 builder.Services.AddDbContext<BethanysPieShopDbContext>(options => {
     options.UseSqlite(
         builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
@@ -22,12 +27,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseStaticFiles();
+app.UseSession();
 app.MapDefaultControllerRoute();//{controller=Home}/{action=Index}/{id?}
 
-
-// app.MapControllerRoute(
-//     name: "default",
-//     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 DbInitializer.Seed(app);
 app.Run();
